556757be7c0ba6acd522b49637a93e6e204199ae
jdrake16 <stovetops111@gmail.com>
2020-07-17 22:26:25 -0600

    project builds but sequencer isnt connected properly.

--- app/src/main/java/edu/cnm/deepdive/lofistudio/SamplerToggleListener.java
+++ app/src/main/java/edu/cnm/deepdive/lofistudio/SamplerToggleListener.java
@@ -1,5 +1,37 @@
 package edu.cnm.deepdive.lofistudio;
 
-public class SamplerToggleListener {
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ToggleButton;
+import edu.cnm.deepdive.lofistudio.sequencer.Sequencer;
 
+public class SamplerToggleListener implements OnClickListener {
+
+  Sequencer sequencer;
+
+  int beats;
+
+  public SamplerToggleListener(Sequencer sequencer, Context ctx, int samples, int beats) {
+    this.sequencer = sequencer;
+    this.beats = beats;
+  }
+
+  @Override
+  public void onClick(View v) {
+    int buttonId = v.getId();
+
+    int samplerId = buttonId / this.beats;
+    int beatId = buttonId % this.beats;
+    if (!(v instanceof ToggleButton)) {
+      Log.e("SampleToggleListener", "Invalid View type: " + v.getClass());
+      return;
+    }
+    ToggleButton currentButton = (ToggleButton) v;
+    if (currentButton.isChecked())
+      sequencer.enableCell(samplerId, beatId);
+    else
+      sequencer.disableCell(samplerId, beatId);
+  }
 }
